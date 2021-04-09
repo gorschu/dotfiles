9ff8c015a683776d6e12cea3a3305968bdeee266
Gordon Schulz <gordon@gordonschulz.de>
2021-04-09 10:12:23 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e # -e: exit on error
+
+if [ ! "$(command -v chezmoi)" ]; then
+  bin_dir="$HOME/.local/bin"
+  chezmoi="$bin_dir/chezmoi"
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  else
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
+  fi
+else
+  chezmoi=chezmoi
+fi
+
+# import and trust our GPG Key
+GPGKEY=DEE550054AA972F6
+GPGKEY_FINGERPRINT=0A47650A15E4F0F4003EC450DEE550054AA972F6
+gpg --keyserver keyserver.ubuntu.com --receive-keys ${GPGKEY}
+echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key ${GPGKEY_FINGERPRINT} trust
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+# exec: replace current process with chezmoi init
+exec "$chezmoi" init --apply "--source=$script_dir"
