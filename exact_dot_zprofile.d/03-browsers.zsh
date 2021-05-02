0b4d2a5ba42ea086098e416593ab97d4f51d2326
Gordon Schulz <gordon@gordonschulz.de>
2021-05-02 11:48:48 +0200

    Make Firefox the default

--- exact_dot_zprofile.d/03-browsers.zsh
+++ exact_dot_zprofile.d/03-browsers.zsh
@@ -1,8 +1,8 @@
 # set browser environment variable
-if [ -e /usr/bin/google-chrome-stable ]; then
+if [ -e /usr/bin/firefox ]; then
+    export BROWSER="/usr/bin/firefox"
+elif [ -e /usr/bin/google-chrome-stable ]; then
     export BROWSER="/usr/bin/google-chrome-stable"
 elif [ -e /usr/bin/google-chrome-beta ]; then
     export BROWSER="/usr/bin/google-chrome-beta"
-elif [ -e /usr/bin/firefox ]; then
-    export BROWSER="/usr/bin/firefox"
 fi
