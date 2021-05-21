b48dd6dcc345712aed95fe851f809a6eb0c45768
Gordon Schulz <gordon@gordonschulz.de>
2021-05-21 08:24:18 +0200

    Kitty is still the best, no doubt

--- exact_dot_zprofile.d/02-terminals.zsh
+++ exact_dot_zprofile.d/02-terminals.zsh
@@ -3,10 +3,10 @@ if expr "$XDG_CURRENT_DESKTOP" : ".*KDE.*" 1>/dev/null && [ -e /usr/bin/konsole 
     export TERMINAL=konsole
 elif expr "$XDG_CURRENT_DESKTOP" : ".*GNOME.*" 1>/dev/null && [ -e /usr/bin/gnome-terminal ]; then
     export TERMINAL=gnome-terminal
-elif [ -e /usr/bin/alacritty ]; then
-    export TERMINAL="alacritty"
 elif [ -e /usr/bin/kitty ] || [ -e "${HOME}/.local/bin/kitty" ]; then
     export TERMINAL="kitty"
+elif [ -e /usr/bin/alacritty ]; then
+    export TERMINAL="alacritty"
 elif [ -e /usr/local/bin/st ] || [ -e /usr/bin/st ]; then
     export TERMINAL=st
 elif [ -e /usr/bin/tilix ]; then
