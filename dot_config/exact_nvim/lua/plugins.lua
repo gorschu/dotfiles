59ffa11a32338cf30bf5584b36cf0fca40a5c263
Gordon Schulz <gordon@gordonschulz.de>
2021-06-28 17:24:39 +0200

    Cleanup plugins

--- dot_config/exact_nvim/lua/plugins.lua
+++ dot_config/exact_nvim/lua/plugins.lua
@@ -1,5 +1,4 @@
 return require('packer').startup(function()
-
     -- Packer can manage itself as an optional plugin
     use {'wbthomason/packer.nvim', opt = false}
 
@@ -10,59 +9,70 @@ return require('packer').startup(function()
     use { 'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'},
         {'nvim-lua/plenary.nvim'}} }
 
-    -- use { 'vim-airline/vim-airline', requires = { { 'vim-airline/vim-airline-themes'} }
-    -- }
+    -- statusline, tabline
     use { 'hoob3rt/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true} }
     use { 'akinsho/nvim-bufferline.lua', requires = 'kyazdani42/nvim-web-devicons'}
+
+    -- treesitter syntax highlighting
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', requires = { { 'nvim-treesitter/nvim-treesitter-textobjects' } } }
 
     -- LSP and completion
     use { 'neovim/nvim-lspconfig' }
     -- use { 'nvim-lua/completion-nvim' } use { 'glepnir/lspsaga.nvim' }
     use { 'hrsh7th/nvim-compe' }
-    use { 'hrsh7th/vim-vsnip', requires = { { 'hrsh7th/vim-vsnip-integ' }, { 'rafamadriz/friendly-snippets' } } }
-    use { 'windwp/nvim-autopairs' }
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', requires = { { 'nvim-treesitter/nvim-treesitter-textobjects' } } }
 
+    -- snippets
+    use { 'hrsh7th/vim-vsnip', requires = { { 'hrsh7th/vim-vsnip-integ' }, { 'rafamadriz/friendly-snippets' } } }
+
+    -- automatic braces
+    use { 'windwp/nvim-autopairs' }
+    -- rainbow braces
+    use { 'p00f/nvim-ts-rainbow '}
+
+    -- file explorer
+    use { 'kyazdani42/nvim-tree.lua', requires = { { 'kyazdani42/nvim-web-devicons' } } }
+    -- tag management
     use { 'ludovicchabant/vim-gutentags' } use { 'majutsushi/tagbar', opt = true }
+
     -- Go
     use { 'fatih/vim-go', opt = true }
 
+    -- git
     use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+    use { 'tpope/vim-fugitive' }
 
+    -- show indentation levels
     use { 'lukas-reineke/indent-blankline.nvim', branch = "lua"}
 
+    -- automatically change cwd to detected roots
     use { 'airblade/vim-rooter' }
 
-    use { 'kyazdani42/nvim-tree.lua', requires = { { 'kyazdani42/nvim-web-devicons' } } }
-
-    use { 'tpope/vim-fugitive' }
+    -- misc plugins
     use { 'tpope/vim-repeat' }
-    use {'tpope/vim-unimpaired'}
+    use { 'tpope/vim-unimpaired'}
     use { 'tpope/vim-surround'}
     use { 'tpope/vim-dispatch' }
-    use {'tpope/vim-vinegar'}
+    use { 'tpope/vim-vinegar'}
     use { 'ntpeters/vim-better-whitespace' }
     use { 'rhysd/clever-f.vim' }
-
-
-    use { 'dense-analysis/ale' }
-
     use { 'jamessan/vim-gnupg' }
 
-    use { 'junegunn/vim-easy-align' }
+    -- linting
+    use { 'dense-analysis/ale' }
 
+
+    -- align stuff, move faster, comments
+    use { 'junegunn/vim-easy-align' }
+    use { 'easymotion/vim-easymotion' }
     use { 'b3nj5m1n/kommentary' }
 
-    use { 'easymotion/vim-easymotion' }
-
+    -- markdown
     use {'iamcco/markdown-preview.nvim', config = "vim.call('mkdp#util#install')"}
 
+    -- tmux integration
     use {'christoomey/vim-tmux-navigator'}
     use {'tmux-plugins/vim-tmux-focus-events'}
 
-    use { 'p00f/nvim-ts-rainbow '}
-
     -- detect python venvs
     use { 'rafi/vim-venom' }
-
 end)
