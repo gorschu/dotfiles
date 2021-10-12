f09712327d8210f8064fec1baa28e7f359018f3a
Gordon Schulz <gordon@gordonschulz.de>
2021-10-12 18:40:03 +0200

    Write initial chezmoi.toml to bootstrap encryption
    If not, we are facing a chicken-egg-problem. We want to decrypt out
    private variables, but chezmoi doesn't know about any encryption method
    yet.

--- install.sh
+++ install.sh
@@ -36,6 +36,17 @@ fi
 # import and trust our GPG Key
 GPGKEY=DEE550054AA972F6
 GPGKEY_FINGERPRINT=0A47650A15E4F0F4003EC450DEE550054AA972F6
+
+# write initial .chezmoi.toml so encryption works
+[[ ! -d $HOME/.config/chezmoi ]] && mkdir -p $HOME/.config/chezmoi
+cat << EOF >> $HOME/.config/chezmoi/chezmoi.toml
+encryption = "gpg"
+
+[gpg]
+recipient = "0x${GPGKEY}"
+suffix = ".asc"
+EOF
+
 gpg --keyserver keyserver.ubuntu.com --receive-keys ${GPGKEY}
 echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key ${GPGKEY_FINGERPRINT} trust
 # power up yubikey
