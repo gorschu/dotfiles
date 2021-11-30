084b41432dd213d192292eb15c37bdee9dc6e934
Gordon Schulz <gordon@gordonschulz.de>
2021-11-30 17:07:33 +0100

    Update bluetooth connect scripts

--- bin/exact_media/executable_bluetooth-connect-headset-msbc.sh
+++ bin/exact_media/executable_bluetooth-connect-headset-msbc.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
-# hardcoded to Jabra Elite 85t for now
+
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <65|85t>"
+    exit 1
+fi
 
 latency=111000
 
-bluetoothctl connect 30:50:75:5E:F6:20 && \
-    pactl set-card-profile bluez_card.30_50_75_5E_F6_20 headset-head-unit-msbc && \
+case $1 in
+65)
+        btid="50:1A:A5:F6:ED:65"
+    ;;
+85t)
+        btid="30:50:75:5E:F6:20"
+    ;;
+*)
+    echo "Invalid headset"
+    exit 1
+    ;;
+esac
+
+btid_underscore=${btid//:/_}
+source_volume_85t=30
+source_volume_65=100
+
+bluetoothctl connect "${btid}" && \
+    pactl set-card-profile bluez_card."${btid_underscore}" headset-head-unit-msbc && \
     sleep 2 && \
     pactl set-port-latency-offset \
-        bluez_card.30_50_75_5E_F6_20 headset-output "${latency}" && \
-    pactl set-default-sink bluez_output.30_50_75_5E_F6_20.headset-head-unit && \
-    pactl set-sink-volume bluez_output.30_50_75_5E_F6_20.headset-head-unit 85% && \
-    pactl set-default-source bluez_input.30_50_75_5E_F6_20.headset-head-unit && \
-    pactl set-source-volume bluez_input.30_50_75_5E_F6_20.headset-head-unit 40%
+        bluez_card."${btid_underscore}" headset-output "${latency}" && \
+    pactl set-default-sink bluez_output."${btid_underscore}".headset-head-unit && \
+    pactl set-sink-volume bluez_output."${btid_underscore}".headset-head-unit 85% && \
+    pactl set-default-source bluez_input."${btid_underscore}".headset-head-unit
+
+case $1 in
+65)
+        pactl set-source-volume bluez_input."${btid_underscore}".headset-head-unit \
+            "$source_volume_65"%
+        easyeffects --load-preset "Jabra_Evolve_65-Calls"
+    ;;
+85t)
+        pactl set-source-volume bluez_input."${btid_underscore}".headset-head-unit \
+            "$source_volume_85t"%
+        easyeffects --load-preset "Jabra_Elite_85t-Calls"
+esac
--- bin/exact_media/executable_bluetooth-connect-headset-sbcxq.sh
+++ bin/exact_media/executable_bluetooth-connect-headset-sbcxq.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
-# hardcoded to Jabra Elite 85t for now
+
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <65|85t>"
+    exit 1
+fi
 
 latency=111000
 
-bluetoothctl connect 30:50:75:5E:F6:20 && \
-    pactl set-card-profile bluez_card.30_50_75_5E_F6_20 a2dp-sink-sbc_xq && \
+case $1 in
+65)
+        btid="50:1A:A5:F6:ED:65"
+    ;;
+85t)
+        btid="30:50:75:5E:F6:20"
+    ;;
+*)
+    echo "Invalid headset"
+    exit 1
+    ;;
+esac
+
+btid_underscore=${btid//:/_}
+
+bluetoothctl connect "${btid}" && \
+    pactl set-card-profile bluez_card."${btid_underscore}" a2dp-sink-sbc_xq && \
     sleep 2 && \
     pactl set-port-latency-offset \
-        bluez_card.30_50_75_5E_F6_20 headset-output "${latency}" && \
-    pactl set-default-sink bluez_output.30_50_75_5E_F6_20.a2dp-sink && \
-    pactl set-sink-volume bluez_output.30_50_75_5E_F6_20.a2dp-sink 60%
+        bluez_card."${btid_underscore}" headset-output "${latency}" && \
+    pactl set-default-sink bluez_output."${btid_underscore}".a2dp-sink && \
+    pactl set-sink-volume bluez_output."${btid_underscore}".a2dp-sink 60%
