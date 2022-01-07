075cbbf4403502133efb465650442c36ae62c50a
Gordon Schulz <gordon@gordonschulz.de>
2022-01-07 13:03:06 +0100

    Ongoing move to Nord - once again
    And for all?

--- exact_dot_zshrc.d/fzf.zsh
+++ exact_dot_zshrc.d/fzf.zsh
@@ -1,8 +1,8 @@
 # always use tmux split panes
 FZF_TMUX=1
 
-# gruvbox
+# nord
 export FZF_DEFAULT_OPTS="
-    --color fg:#ebdbb2,bg:#282828,hl:#fabd2f,fg+:#ebdbb2,bg+:#3c3836,hl+:#fabd2f
-    --color info:#83a598,prompt:#bdae93,spinner:#fabd2f,pointer:#83a598,marker:#fe8019,header:#665c54
+    --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+    --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
 "
