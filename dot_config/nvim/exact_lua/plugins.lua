1d10836919e137a669a4a31564f2b23b711f1bd2
Gordon Schulz <gordon@gordonschulz.de>
2022-01-25 11:19:43 +0100

    Add lazygit

--- dot_config/nvim/exact_lua/plugins.lua
+++ dot_config/nvim/exact_lua/plugins.lua
@@ -40,8 +40,9 @@ return require('packer').startup(function()
 
     -- git
     use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
-    use { 'TimUntersberger/neogit', 
-        requires = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' }}
+    use { 'kdheepak/lazygit.nvim' }
+    use { 'TimUntersberger/neogit',
+           requires = { 'nvim-lua/plenary.nvim', 'sindrets/diffview.nvim' }}
     use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
     -- show indentation levels
