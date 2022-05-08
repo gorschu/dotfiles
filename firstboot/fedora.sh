87bb991b5fa35e6901f7fa3204c78d7da518952a
Gordon Schulz <gordon@gordonschulz.de>
2022-05-08 12:42:35 +0200

    Update fedora first boot script

--- firstboot/fedora.sh
+++ firstboot/fedora.sh
@@ -6,15 +6,15 @@ POOL=dpool
 zedtrigger=dpool/libvirt
 restorecon=("/home" "/var/lib/libvirt")
 
-if ! rpm -q zfs > /dev/null; then
-  sudo dnf -y install https://zfsonlinux.org/fedora/zfs-release$(rpm -E %dist).noarch.rpm
+if ! rpm -q zfs >/dev/null; then
+  sudo dnf -y install "https://zfsonlinux.org/fedora/zfs-release$(rpm -E %dist).noarch.rpm"
   sudo dnf -y install -y kernel-devel && sudo dnf swap -y zfs-fuse zfs
-  echo "zfs" | sudo tee /etc/modules-load.d/zfs.conf > /dev/null
+  echo "zfs" | sudo tee /etc/modules-load.d/zfs.conf >/dev/null
 fi
 sudo modprobe zfs
 
-if ! sudo zpool list -o name,health -H | grep -i "^${POOL}.*ONLINE; then
-	sudo zpool export ${POOL} && sudo zpool import ${POOL}
+if ! sudo zpool list -o name,health -H | grep -i -q "^${POOL}.*ONLINE"; then
+  sudo zpool import ${POOL}
 fi
 sudo zpool set cachefile=/etc/zfs/zpool.cache ${POOL}
 sudo systemctl enable zfs-import-cache.service zfs-import.target
@@ -25,12 +25,12 @@ sudo systemctl enable --now zfs.target zfs-zed.service zfs-scrub-monthly@${POOL}
 sudo zfs set canmount=off ${zedtrigger}
 sudo zfs set canmount=on ${zedtrigger}
 
-if ! sudo zfs get -o value -H keystatus ${POOL} | grep "^available"; then
-	sudo zfs load-key -L $(zfs get -o value -H keylocation ${POOL}) ${POOL}
+if ! sudo zfs get -o value -H keystatus ${POOL} | grep -q "^available"; then
+  sudo zfs load-key -L "$(zfs get -o value -H keylocation ${POOL})" ${POOL}
 fi
+sudo chown root:root "$(zfs get -o value -H keylocation ${POOL} | sed -r 's/^file:\/\/(.*)$/\1/')"
+sudo chmod 600 "$(zfs get -o value -H keylocation ${POOL} | sed -r 's/^file:\/\/(.*)$/\1/')"
+
 sudo zfs mount -a
 
 for path in "${restorecon[@]}"; do sudo restorecon -R "${path}"; done
-
-
-
