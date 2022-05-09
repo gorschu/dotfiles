b58b8a733d10a3f1eb0748d882c72eab1c365e36
Gordon Schulz <gordon@gordonschulz.de>
2022-05-09 12:08:21 +0200

    Add Webcam settings script

---
+++ bin/exact_media/executable_v4l2-c922-set.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+device=$(v4l2-ctl --list-devices | grep -A 1 "^C922" | tail -1 | sed -r 's/^[ ]+//g')
+
+v4l2-ctl --device ${device} --set-ctrl backlight_compensation=1
+v4l2-ctl --device ${device} --set-fmt-video=width=1920,height=1080,pixelformat=MJPG
+# set framerate
+v4l2-ctl --device ${device} --set-parm=30
