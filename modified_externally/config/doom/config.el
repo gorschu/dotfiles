f373656a8b72a2edcfe946ea087be5e3390bf7c6
Gordon Schulz <gordon@gordonschulz.de>
2022-11-10 07:57:25 +0100

    Update files

--- modified_externally/config/doom/config.el
+++ modified_externally/config/doom/config.el
@@ -5,41 +5,58 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "Gordon Schulz"
       user-mail-address "gordon@gordonschulz.de")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "JetBrains Mono" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Sans" :size 13 :weight: 'regular)
-      )
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Iosevka SS14" :size 14 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-nord)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq doom-theme 'doom-gruvbox)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
 
-;; Here are some additional functions/macros that could help you configure Doom:
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -52,40 +69,10 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-;; Don't open a new workspace on emacsclient invocation
-(after! persp-mode
-  (setq persp-emacsclient-init-frame-behaviour-override "main"))
-
-;; don't output stuff to stdout after emacsclient invocation
-;; we filter the print-stuff here
-(define-advice server-eval-and-print (:filter-args (args) no-print)
-  (list (car args) nil))
-
-(doom-load-envvars-file (concat doom-private-dir "env"))
-
-(setq deft-extensions '("txt" "md" "org"))
-(setq deft-directory "~/org")
-(setq deft-recursive t)
-(setq deft-use-filename-as-title t)
-
-;; use all files in ~/org to be used in agenda
-;;(after! org
-  (setq org-directory "~/org")
-  (setq org-roam-directory "~/org/roam")
-;;  (setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
-;;)
-
-(setq org-refile-targets
-      '((nil :maxlevel . 3)
-        (org-agenda-files :maxlevel . 1)))
-(setq org-attach-id-dir (concat (file-name-as-directory org-directory) "attachments"))
-
-;; add .asc to gpg filenames to be automatically decrypted
-(setq epa-file-name-regexp "\\.gpg\\(~\\|\\.~[0-9]+~\\)?\\'\\|\\.asc")
-(epa-file-name-regexp-update)
-
-(setq treemacs-git-mode 'deferred)
+(setq org-log-done 'time)
