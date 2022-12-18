db88fa876781ac72c49e1c7cd41be72aef6ddd61
Gordon Schulz <gordon@gordonschulz.de>
2022-12-18 13:02:43 +0100

    Fix chezmoi installation
    1password needs chezmoi root:root with 0755 for biometric authentication

--- install.sh
+++ install.sh
@@ -21,16 +21,13 @@ elif [[ $(grep "^ID" /etc/os-release) =~ opensuse ]]; then
 fi
 
 if [ ! "$(command -v chezmoi)" ]; then
-  bin_dir="$HOME/.local/bin"
+  bin_dir="/usr/local/bin"
+  bin_dir_tmp=$(mktemp -d)
   chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+  # chezmoi needs to be root:root owned for 1password to allow it for binary permission verification
+  # which does make a lot of sense from a security perspective - therefore move it to /usr/local/bin
+  sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir_tmp" && \
+    sudo install --owner root --group root --mode 755 --compare ${bin_dir_tmp}/chezmoi ${chezmoi}
 else
   chezmoi=chezmoi
 fi
