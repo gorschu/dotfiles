6d68c1c92cd6df59f39e13325827f6c5556583ec
Gordon Schulz <gordon@gordonschulz.de>
2023-01-19 22:04:11 +0100

    Remove old nvim configuration

--- dot_config/nvim/exact_lua/exact_plugins/gutentags.lua
+++
@@ -1,8 +0,0 @@
-vim.g.gutentags_define_advanced_commands = 1
-vim.g.gutentags_cache_dir = vim.loop.os_homedir() .. '/.cache/gutentags'
-vim.g.gutentags_generate_on_new = 1
-vim.g.gutentags_generate_on_missing = 1
-vim.g.gutentags_generate_on_write = 1
-vim.g.gutentags_generate_on_empty_buffer = 0
-vim.g.gutentags_file_list_command = 'rg --files'
-
