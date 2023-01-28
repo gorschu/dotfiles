03556897cc19e7ce93e29e83c47177fd37ce4999
Gordon Schulz <gordon@gordonschulz.de>
2023-01-28 14:14:43 +0100

    WIP

--- dot_config/astronvim/lua/exact_user/init.lua
+++ dot_config/astronvim/lua/exact_user/init.lua
@@ -300,14 +300,20 @@ local config = {
 			"jose-elias-alvarez/null-ls.nvim",
 			opts = function(_, config)
 				-- config variable is the default configuration table for the setup function call
-				-- local null_ls = require "null-ls"
+				local null_ls = require "null-ls"
 
 				-- Check supported formatters and linters
 				-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 				-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 				config.sources = {
 					-- Set a formatter
-					-- null_ls.builtins.formatting.stylua,
+					-- lua
+					null_ls.builtins.formatting.stylua,
+					-- python
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.diagnostics.pylint,
+					null_ls.builtins.diagnostics.mypy,
 					-- null_ls.builtins.formatting.prettier,
 				}
 				return config -- return final config table
@@ -324,7 +330,7 @@ local config = {
 			"williamboman/mason-lspconfig.nvim",
 			-- overrides `require("mason-lspconfig").setup(...)`
 			opts = {
-				-- ensure_installed = { "sumneko_lua" },
+				ensure_installed = { "sumneko_lua", "pyright", },
 			},
 		},
 		-- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
