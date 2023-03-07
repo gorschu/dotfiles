d82b1904e4fd5db265336001dab45f5ec8c2d42b
Gordon Schulz <gordon@gordonschulz.de>
2023-03-07 12:25:25 +0100

    Don't merge .Xresources under WAYLAND_DISPLAY

--- exact_dot_zprofile.d/04-xresources.zsh
+++ exact_dot_zprofile.d/04-xresources.zsh
@@ -1,3 +1,5 @@
 # merge in .Xresources, GDM and co. don't like includes
-[ -n "${DISPLAY}" ] && [ -f "$HOME/.Xresources" ] &&
-	xrdb -merge -I"$HOME" "$HOME/.Xresources"
+[ -n "${DISPLAY}" ] && [[ -z WAYLAND_DISPLAY ]] && [ -f "$HOME/.Xresources" ] &&
+  xrdb -merge -I"$HOME" "$HOME/.Xresources"
+
+# vim: ft=sh
