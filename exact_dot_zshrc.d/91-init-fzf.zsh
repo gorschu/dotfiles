ecc61b0c2ded6fba6682ab58a7e3c7429fc628b6
Gordon Schulz <gordon@gordonschulz.de>
2023-04-23 18:41:50 +0200

    refactor: Rework initialization of custom ZSH variables/inits

---
+++ exact_dot_zshrc.d/91-init-fzf.zsh
@@ -0,0 +1,6 @@
+# source in fzf
+# fedora
+[ -f /usr/share/fzf/shell/key-bindings.zsh ] && source /usr/share/fzf/shell/key-bindings.zsh
+# custom install via install script
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
