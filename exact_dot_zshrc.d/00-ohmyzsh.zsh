5dad7fbc22cca98c5ef967dc1a7c57cf8e1c37fc
Gordon Schulz <gordon@gordonschulz.de>
2023-04-23 15:45:27 +0200

    refactor: Remove obsolete omz settings now handled by prezto

--- exact_dot_zshrc.d/00-ohmyzsh.zsh
+++
@@ -1,8 +0,0 @@
-# disable ohmyzsh auto-updates; zgenom autoupdate covers it
-DISABLE_AUTO_UPDATE="true"
-# disable ohmyzsh AUTOCORRECTION
-ENABLE_CORRECTION="false"
-# let oh-my-zsh set the terminal title
-DISABLE_AUTO_TITLE=false
-# disabled dots while waiting for completion
-COMPLETION_WAITING_DOTS=true
