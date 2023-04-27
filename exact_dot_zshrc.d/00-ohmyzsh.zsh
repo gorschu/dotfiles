e0a95c81cfc37538566821a534a15488031d7400
Gordon Schulz <gordon@gordonschulz.de>
2023-04-27 22:37:58 +0200

    fix: Back to zgenom and omz

---
+++ exact_dot_zshrc.d/00-ohmyzsh.zsh
@@ -0,0 +1,8 @@
+# disable ohmyzsh auto-updates; zgenom autoupdate covers it
+DISABLE_AUTO_UPDATE="true"
+# disable ohmyzsh AUTOCORRECTION
+ENABLE_CORRECTION="false"
+# let oh-my-zsh set the terminal title
+DISABLE_AUTO_TITLE=false
+# disabled dots while waiting for completion
+COMPLETION_WAITING_DOTS=true
