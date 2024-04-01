db9aa8b813322e4221f7366365a22a6d170ddcd7
Gordon Schulz <gordon@gordonschulz.de>
2024-04-01 11:50:29 +0200

    chore(chezmoi): Update install script

--- install.sh
+++ install.sh
@@ -9,8 +9,9 @@ reset=$(tput sgr0)
 echo "${green}Installing absolute requirements${reset}"
 if [[ $(grep "^ID" /etc/os-release) =~ fedora ]]; then
   sudo dnf install -y git yubikey-manager
+  sudo dnf install -y $(curl -sL https://api.github.com/repos/twpayne/chezmoi/releases/latest | jq -r '.assets[] | select(.name? | match("chezmoi-.*-x86_64.rpm$")) | .browser_download_url')
 elif [[ $(grep "^ID" /etc/os-release) =~ opensuse ]]; then
-  sudo zypper install -y git yubikey-manager
+  sudo zypper install -y git yubikey-manager chezmoi
 elif [[ $(grep "^ID" /etc/os-release) =~ arch ]]; then
   sudo pacman -S --needed -y git yubikey-manager chezmoi gnupg
 else
@@ -19,17 +20,13 @@ fi
 
 chezmoi="chezmoi"
 if [ ! "$(command -v chezmoi)" ]; then
-  if [[ $(grep "^ID" /etc/os-release) =~ opensuse ]]; then
-    sudo zypper install -y chezmoi
-  else
-    bin_dir="/usr/local/bin"
-    bin_dir_tmp=$(mktemp -d)
-    chezmoi="$bin_dir/chezmoi"
-    # chezmoi needs to be root:root owned for 1password to allow it for binary permission verification
-    # which does make a lot of sense from a security perspective - therefore move it to /usr/local/bin
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir_tmp" &&
-      sudo install --owner root --group root --mode 755 --compare "${bin_dir_tmp}/chezmoi" "${chezmoi}"
-  fi
+  bin_dir="/usr/local/bin"
+  bin_dir_tmp=$(mktemp -d)
+  chezmoi="$bin_dir/chezmoi"
+  # chezmoi needs to be root:root owned for 1password to allow it for binary permission verification
+  # which does make a lot of sense from a security perspective - therefore move it to /usr/local/bin
+  sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir_tmp" &&
+    sudo install --owner root --group root --mode 755 --compare "${bin_dir_tmp}/chezmoi" "${chezmoi}"
 fi
 
 # stop pcscd for first use - it conflicts with gnupg when the latter is not configured to use it
