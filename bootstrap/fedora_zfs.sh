a6844c0b9c8391e8368c0630d68620869a0a66a2
Gordon Schulz <gordon@gordonschulz.de>
2024-06-10 08:10:48 +0200

    refactor(zfs): Update fedora bootstrap script

--- bootstrap/fedora_zfs.sh
+++ bootstrap/fedora_zfs.sh
@@ -13,81 +13,82 @@ operation=${1:-none}
 disk=${2:-none}
 partno=9
 
-installZFS () {
-    if ! rpm -q zfs >/dev/null; then
-       #dnf install -y https://zfsonlinux.org/fedora/zfs-release-2-2$(rpm --eval "%{dist}").noarch.rpm
-       dnf install -y https://zfsonlinux.org/fedora/zfs-release-2-2.fc37.noarch.rpm
-       dnf -y install -y kernel-devel &&  dnf swap -y zfs-fuse zfs
-      echo "zfs" |  tee /etc/modules-load.d/zfs.conf >/dev/null
-    fi
-     modprobe zfs
+installZFS() {
+  if ! rpm -q zfs >/dev/null; then
+    rpm -e --nodeps zfs-fuse || true
+    dnf install -y https://zfsonlinux.org/fedora/zfs-release-2-5"$(rpm --eval "%{dist}")".noarch.rpm
+    dnf -y install -y kernel-devel && dnf -y install zfs
+    echo "zfs" | tee /etc/modules-load.d/zfs.conf >/dev/null
+    echo "zfs" >/etc/dnf/protected.d/zfs.conf
+  fi
+  modprobe zfs
 }
 
-createPartition () {
-    ! lsmod | grep -q zfs && \
-      installZFS && \
-      ! lsmod | grep -q zfs && echo "zfs module not loaded!" && exit 1
-
-    [[ ! -L ${disk} ]] && echo "${disk} is not a valid disk device link!" && exit 1
-    [[ -L ${disk}-part${partno} ]] && echo "${disk}-part${partno} already exists!" && exit 1
-    
-     mkdir -p /etc/zfs
-    [[ ! -f "/etc/zfs/zfskey_${pool}_$(hostname)" ]] && \
-     openssl rand -hex -out "/etc/zfs/zfskey_${pool}_$(hostname)" 32 && \
-         chown root:root "/etc/zfs/zfskey_${pool}_$(hostname)" && \
-         chmod 600 "/etc/zfs/zfskey_${pool}_$(hostname)"
-    
-    sgdisk --new=9:0:0 -c 9:"zfs ${pool}" -t 3:bf01 "${disk}"
-    partprobe
-    sleep 2
-    wipefs -a ${disk}-part${partno}
-    
-     zpool create \
-      -o ashift=12 \
-      -o autotrim=on \
-      -O encryption=aes-256-gcm \
-      -O keylocation="file:///etc/zfs/zfskey_${pool}_$(hostname)" \
-      -O keyformat=hex \
-      -O acltype=posixacl \
-      -O compression=zstd \
-      -O dnodesize=auto \
-      -O normalization=formD \
-      -O relatime=on \
-      -O xattr=sa \
-      -O canmount=off \
-      -O mountpoint=/ \
-      -R /tmp/${pool} \
-      ${pool} \
-      ${disk}-part${partno}
-     
-    zfs create -p -o mountpoint=/home/${user} ${pool}/home/${user} && \
-      sudo chown -R ${user}:${user} /tmp/${pool}/home/${user}
-}
-
-importPool () {
-  ! lsmod | grep -q zfs && \
-    installZFS && \
+createPartition() {
+  ! lsmod | grep -q zfs &&
+    installZFS &&
     ! lsmod | grep -q zfs && echo "zfs module not loaded!" && exit 1
 
-  if !  zpool list -o name,health -H | grep -i -q "^${pool}.*ONLINE"; then
-     zpool import ${pool}
+  [[ ! -L ${disk} ]] && echo "${disk} is not a valid disk device link!" && exit 1
+  [[ -L ${disk}-part${partno} ]] && echo "${disk}-part${partno} already exists!" && exit 1
+
+  mkdir -p /etc/zfs
+  [[ ! -f "/etc/zfs/zfskey_${pool}_$(hostname)" ]] &&
+    openssl rand -hex -out "/etc/zfs/zfskey_${pool}_$(hostname)" 32 &&
+    chown root:root "/etc/zfs/zfskey_${pool}_$(hostname)" &&
+    chmod 600 "/etc/zfs/zfskey_${pool}_$(hostname)"
+
+  sgdisk --new=9:0:0 -c 9:"zfs ${pool}" -t 3:bf01 "$disk"
+  partprobe
+  sleep 2
+  wipefs -a "$disk-part$partno"
+
+  zpool create \
+    -o ashift=12 \
+    -o autotrim=on \
+    -O encryption=aes-256-gcm \
+    -O keylocation="file:///etc/zfs/zfskey_${pool}_$(hostname)" \
+    -O keyformat=hex \
+    -O acltype=posixacl \
+    -O compression=zstd \
+    -O dnodesize=auto \
+    -O normalization=formD \
+    -O relatime=on \
+    -O xattr=sa \
+    -O canmount=off \
+    -O mountpoint=/ \
+    -R /tmp/"$pool" \
+    "$pool" \
+    "$disk-part$partno"
+
+  zfs create -p -o mountpoint=/home/"$user" "$pool/home/$user" &&
+    sudo chown -R "$user:$user" /tmp/"$pool/home/$user"
+}
+
+importPool() {
+  ! lsmod | grep -q zfs &&
+    installZFS &&
+    ! lsmod | grep -q zfs && echo "zfs module not loaded!" && exit 1
+
+  if ! zpool list -o name,health -H | grep -i -q "^${pool}.*ONLINE"; then
+    zpool import "$pool"
   fi
-  zpool set cachefile=/etc/zfs/zpool.cache ${pool}
+  zpool set cachefile=/etc/zfs/zpool.cache "$pool"
   systemctl enable zfs-import-cache.service zfs-import.target
   mkdir -p /etc/zfs/zfs-list.cache
-  systemctl enable --now zfs.target zfs-zed.service zfs-scrub-monthly@${pool}.timer
-  [[ ! -e /etc/zfs/zfs-list.cache/${pool} ]] &&  touch /etc/zfs/zfs-list.cache/${pool}
-  
-  zfs set canmount=off ${zedtrigger}
-  zfs set canmount=on ${zedtrigger}
-  
-  if !  zfs get -o value -H keystatus ${pool} | grep -q "^available"; then
-     zfs load-key -L "$(zfs get -o value -H keylocation ${pool})" ${pool}
+  systemctl enable --now zfs.target zfs-zed.service zfs-scrub-monthly@"$pool".timer
+  [[ ! -e /etc/zfs/zfs-list.cache/${pool} ]] && touch /etc/zfs/zfs-list.cache/"$pool"
+
+  zfs set canmount=off "$zedtrigger"
+  zfs set canmount=on "$zedtrigger"
+
+  if ! zfs get -o value -H keystatus "$pool" | grep -q "^available"; then
+    zfs load-key -L "$(zfs get -o value -H keylocation "$pool")" "$pool"
   fi
 
-  chown root:root "$(zfs get -o value -H keylocation ${pool} | sed -r 's/^file:\/\/(.*)$/\1/')"
-  chmod 600 "$(zfs get -o value -H keylocation ${pool} | sed -r 's/^file:\/\/(.*)$/\1/')"
-  
+  chown root:root "$(zfs get -o value -H keylocation "$pool" | sed -r 's/^file:\/\/(.*)$/\1/')"
+  chmod 600 "$(zfs get -o value -H keylocation "$pool" | sed -r 's/^file:\/\/(.*)$/\1/')"
+
   echo "While logged in as root, do: "
   echo "rsync -vau /home/${user}/ /tmp/${pool}/home/${user}/"
   echo "rm -rf /home"
@@ -98,16 +99,18 @@ importPool () {
 }
 
 case ${operation} in
-  partcreate)
-    [[ ${disk} == 'none' ]] && echo "Disk to perform operation on not supplied." && echo "usage: $0 partcreate /dev/disk/by-id/...." && exit 1
-    createPartition
+install)
+  installZFS
   ;;
-  importpool)
-    importPool
+partcreate)
+  [[ ${disk} == 'none' ]] && echo "Disk to perform operation on not supplied." && echo "usage: $0 partcreate /dev/disk/by-id/...." && exit 1
+  createPartition
   ;;
-  *)
-    echo "Please invoke with partcreate or importpool."
-    exit 1
+importpool)
+  importPool
+  ;;
+*)
+  echo "Please invoke with install, partcreate or importpool."
+  exit 1
   ;;
 esac
-
