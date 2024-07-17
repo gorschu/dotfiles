d013fcd314d89f35f166c6695603bb1baa022283
Gordon Schulz <gordon@gordonschulz.de>
2024-07-17 11:40:31 +0200

    chore: Remove obsolute Xresources file

--- dot_zsh/exact_dot_zprofile.d/readonly_04-xresources.zsh
+++
@@ -1,5 +0,0 @@
-# merge in .Xresources, GDM and co. don't like includes
-[ -n "${DISPLAY}" ] && [[ -z WAYLAND_DISPLAY ]] && [ -f "$HOME/.Xresources" ] &&
-  xrdb -merge -I"$HOME" "$HOME/.Xresources"
-
-# vim: ft=sh
