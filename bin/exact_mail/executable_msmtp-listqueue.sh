12b314b218e62c577e7d413d89d4719178469f30
Gordon Schulz <gordon@gordonschulz.de>
2025-10-19 12:49:56 +0200

    chore: remove all unused mail setup

--- bin/exact_mail/executable_msmtp-listqueue.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-QUEUEDIR=${XDG_DATA_HOME}/msmtpqueue
-
-for i in "$QUEUEDIR"/*.mail; do
-    grep -E -s --colour -h '(^From:|^To:|^Subject:)' "$i" || echo "No mail in queue"
-    echo " "
-done
