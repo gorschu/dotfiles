1aa4561539f7293c537fae62906413a335ec8f4e
Gordon Schulz <gordon.schulz@f-i.de>
2025-10-27 11:51:55 +0100

    chore: Remove obsolete wezterm script

--- .chezmoiscripts/run_once_wezterm_terminfo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-tempfile=$(mktemp) &&
-  curl -s -o "$tempfile" https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo &&
-  tic -x -o ~/.terminfo "$tempfile" &&
-  rm -f "$tempfile"
