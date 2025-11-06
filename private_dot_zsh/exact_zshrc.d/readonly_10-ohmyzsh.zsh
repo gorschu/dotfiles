9d3ae256081d9a8184adaa00a1cf8e2d4a4bba66
Gordon Schulz <gordon@gordonschulz.de>
2025-11-06 08:38:29 +0100

    chore: Move omz auto-update disablement to zstyles

--- private_dot_zsh/exact_zshrc.d/readonly_10-ohmyzsh.zsh
+++ private_dot_zsh/exact_zshrc.d/readonly_10-ohmyzsh.zsh
@@ -1,8 +1,6 @@
-# disable ohmyzsh auto-updates; zgenom autoupdate covers it
-DISABLE_AUTO_UPDATE="true"
 # disable ohmyzsh AUTOCORRECTION
 ENABLE_CORRECTION="false"
-# let oh-my-zsh set the terminal title
-DISABLE_AUTO_TITLE=false
-# disabled dots while waiting for completion
+# disable automatic terminal title updates
+DISABLE_AUTO_TITLE=true
+# show dots while waiting for completion
 COMPLETION_WAITING_DOTS=true
