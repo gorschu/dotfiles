0b1a02b9476b6406c0fbbcbeba4a903672026f49
Gordon Schulz <gordon@gordonschulz.de>
2026-02-28 18:31:35 +0100

    fix(kitty,tmux): improve terminal integration, shell integration and cwd tracking

--- private_dot_zsh/exact_zshrc.d/readonly_99-kitty-integration.zsh
+++ private_dot_zsh/exact_zshrc.d/readonly_99-kitty-integration.zsh
@@ -10,4 +10,15 @@ if test -n "$KITTY_INSTALLATION_DIR"; then
     autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
     kitty-integration
     unfunction kitty-integration
+    alias ssh="kitty +kitten ssh"
+
+    # Inside tmux, OSC 7 hits tmux's PTY and never reaches Kitty.
+    # Wrap it in DCS passthrough so Kitty can track cwd for new_*_with_cwd.
+    if [[ -n "$TMUX" ]]; then
+        _kitty_tmux_report_pwd() {
+            printf '\ePtmux;\e\e]7;kitty-shell-cwd://%s%s\a\e\\' "$HOST" "$PWD"
+        }
+        add-zsh-hook chpwd _kitty_tmux_report_pwd
+        add-zsh-hook precmd _kitty_tmux_report_pwd
+    fi
 fi
